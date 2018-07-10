1f61dae8e03710b9574a469b1d8eb9e69e611b93
charleswang234 <wang.charles234@gmail.com>
2018-07-10 15:48:27 +0000

    set initial state

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,43 @@ import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-      <MessageList />
-      <ChatBar />
-      </div>
-      );
+
+  constructor(props) {
+    super(props);
+    this.state = {loading: true,
+      currentUser: {name: "Bob"},
+      messages: [
+      {
+        username: "Bob",
+        content: "Has anyone seen my marbles?",
+      },
+      {
+        username: "Anonymous",
+        content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+      }
+      ]}
+    }
+
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({loading:false});
+      }, 3000)
+    }
+
+
+
+    render() {
+      if (this.state.loading) {
+        return (<h1>Loading...</h1>);
+      } else {
+
+        return (
+          <div>
+          <MessageList />
+          <ChatBar />
+          </div>
+          );
+      }
+    }
   }
-}
-export default App;
+  export default App;
