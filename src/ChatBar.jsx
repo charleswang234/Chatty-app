644a7948f127ec435a52236c9574f8675597efa2
charleswang234 <wang.charles234@gmail.com>
2018-07-11 18:57:56 +0000

    added state to chatbar

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,7 +1,23 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+  state={
+    "username": "Bob",
+    "inputMessage": ""
+  }
+
+  changeUsername = (evt) => {
+    this.setState({"username": evt.target.value});
+  }
+
+  changeMessage = (evt) => {
+    this.setState({inputMessage: evt.target.value});
+  }
+
   render () {
+
+
+
     const submitMessage = evt => {
       if (evt.key == "Enter") {
         const newMessageInput = evt.target;
@@ -10,21 +26,24 @@ class ChatBar extends Component {
       }
     }
 
+
+
     const submitUsername = evt => {
       console.log("inputting");
       if (evt.key == "Enter") {
         console.log("checking");
-        const newUsername = evt.target;
-        console.log(newUsername.value);
-        this.props.addNewUsername(newUsername.value);
+        // const newUsername = evt.target;
+        // console.log(newUsername.value);
+        this.props.addNewUsername(this.state.username);
       }
     }
+
 
     return (
       <footer className="chatbar">
 
-      <input className="chatbar-username" name = "newUserName" onKeyPress = {submitUsername} defaultValue = {this.props.currentUser.name} />
-      <input className="chatbar-message" name = "newMessage" onKeyPress = {submitMessage} placeholder="Type a message and hit ENTER" />
+      <input className="chatbar-username" name = "newUserName" onChange = {this.changeUsername} onKeyDown = {submitUsername} defaultValue = {this.props.currentUser.name} />
+      <input className="chatbar-message" name = "newMessage" onChange = {this.changeMessage} onKeyPress = {submitMessage} placeholder="Type a message and hit ENTER" />
       </footer>
       )
   }
