b5bddd3d1c0035cc14610ae0c980386ad7fdecf1
charleswang234 <wang.charles234@gmail.com>
2018-07-12 21:39:39 +0000

    minor changes

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,30 +1,54 @@
 import React, {Component} from 'react';
 
+const imageUrlTest = /https?:\/\/.*\.(?:png|jpg|gif)/;
+// what.test("str");
+
 class Message extends Component {
 
 
+    // var whatever = "asdfasdf jkasldf http://asdf.gif https://asfdasdfa.jpg"
+
+  // check if filtering is contained within word
+  containedWithin(filtering, word) {
+    return filtering.test(word);
+  }
+
+  newContent(content, filter) {
+    const splitMessage = content.split(" ");
+    const condensedMessage = [];
+    let currentIndex = 0;
+    for (let i = 0; i < splitMessage.length; ++i) {
+      if (containedWithin(filter, splitMessage[i])) {
+        condensedMessage.push(splitMessage[i]);
+      }
+    }
+    return condensedMessage;
+  }
+
   render() {
 
+
+    // {this.newCentent(this.props.content)}
     switch(this.props.type) {
       case "incomingMessage":
-        return (
+      return (
         <div className="message">
-          <span className="message-username" style={{color: this.props.userColour}}>{this.props.username}</span>
-          <span className="message-content">{this.props.content}</span>
+        <span className="message-username" style={{color: this.props.userColour}}>{this.props.username}</span>
+        <span className="message-content">{this.props.content}</span>
         </div>
         );
-        break;
+      break;
       case "incomingNotification":
-        return (
+      return (
         <div className="notification">
-          <span className="notification-content">{this.props.content}</span>
+        <span className="notification-content">{this.props.content}</span>
         </div>
         );
-        break;
+      break;
       default:
         // show an error in the console if the message type is known
         throw new Error("Unknown data type " + data.type);
+      }
     }
   }
-}
-export default Message;
+  export default Message;
